ebc577627503891db8ed96d8f186c7173b87cee2
Hannes Mehnert <hannes@mehnert.org>
2020-01-20 17:45:46 +0100

    print ip address from remote

--- mirage/conduit_mirage.ml
+++ mirage/conduit_mirage.ml
@@ -179,6 +179,9 @@ module TCP (S: Mirage_stack.V4) = struct
   let listen t (`TCP port: server) fn =
     let s, _u = Lwt.task () in
     S.listen_tcpv4 t ~port (fun flow ->
+        let ip, port = S.TCPV4.dst flow in
+        Printf.printf "conduit connection from %s:%d\n%!"
+          (Ipaddr.V4.to_string ip) port;
         let f = Flow.create (module S.TCPV4) flow in
         fn f
       );
